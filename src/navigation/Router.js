35ce8da69ed69e747e55373333d682e78ba6a73b
Salih Baskurt <baskurts@chc.edu>
2024-01-23 21:13:00 -0500

    Second version of Shopper

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from '../screens/Home';
+import TabNavigator from './TabNavigator';
+import AddListScreen from '../screens/AddList';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,8 @@ const Router = props => {
           component={HomeScreen}
           options={{headerShown: false}}
         />
+        <Stack.Screen name={'Start Shopping!'} component={TabNavigator}/>
+        <Stack.Screen name={'Add List'} component={AddListScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/Lists/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './styles';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const ListsScreen = props => {
+
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+        <View style={styles.bottom}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('Add List')}>
+                <Text style={styles.buttonText}>Add List</Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+  );
+};
+
+export default ListsScreen;
